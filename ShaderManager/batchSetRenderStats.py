9cc2fe5850c081e187a397390e0154520f39d290
Balek <ygouraud@gmail.com>
2013-03-29 19:30:28 +0100

    Version 2: User can now chooses which render stats to set instead of being forced to set a value for every stats before pushing "Apply to selected". It can be useful if specific objects has been set up individually and the user does not want these settings to be overriden.

--- ShaderManager/batchSetRenderStats.py
+++ ShaderManager/batchSetRenderStats.py
@@ -1,56 +1,62 @@
 import pymel.core as pc
+from functools import partial
 
 # Dictionnary that contains all UI elements
 uiWidgets = {}
 
+# Global variable 
+elements = ['primaryVisibility', 'visibleInReflections', 'visibleInRefractions', 'castsShadows', 'receiveShadows', 'aiOpaque']
+
 # Explore recursively
-def exploreFurther(selected, visibility, opaque, reflect, refract, castShadows, receiveShadows):
+def exploreFurther(selected, elements):
     relatives = pc.listRelatives(selected)
     
+    # Dig deeper in the hierarchy to find the shapes 
     for relative in relatives:
         if len(pc.listRelatives(relative)) != 0:
-            exploreFurther(relative, visibility, reflect, refract, castShadows, receiveShadows)
+            exploreFurther(relative, elements)
         elif relative.type() == 'mesh':
-            print "Setting primary visibility of {0} to {1} ".format(relative, visibility)
-            relative.setAttr('primaryVisibility', visibility)
-            print "Setting opaque of {0} to {1} ".format(relative, opaque)
-            relative.setAttr('aiOpaque', opaque)
-            print "Setting visible in Reflections of {0} to {1} ".format(relative, reflect)
-            relative.setAttr('visibleInReflections', reflect)
-            print "Setting visible in Refractions of {0} to {1} ".format(relative, refract)
-            relative.setAttr('visibleInRefractions', refract)
-            print "Setting casts Shadows of {0} to {1} ".format(relative, castShadows)
-            relative.setAttr('castsShadows', castShadows)
-            print "Setting receives Shadows of {0} to {1} ".format(relative, receiveShadows)
-            relative.setAttr('receiveShadows', receiveShadows)
+            
+            for element in elements.keys():
+                pc.setAttr(relative+'.'+element, elements[element])
+
             
 def applyToSelected(*args):
     
-    # Value queried from the checkbox
-    visibilityValue = pc.checkBox(uiWidgets['visibility'], q=1, value=1)
-    opaqueValue = pc.checkBox(uiWidgets['opaque'], q=1, value=1)
-    reflectValue = pc.checkBox(uiWidgets['visibleReflect'], q=1, value=1)
-    refractValue = pc.checkBox(uiWidgets['visibleRefract'], q=1, value=1)
-    castShadValue = pc.checkBox(uiWidgets['castShadows'], q=1, value=1)
-    receiveShadValue = pc.checkBox(uiWidgets['receiveShadows'], q=1, value=1)
-
-    # Get the selected objects
-    selections = pc.ls(sl=1)
+    elementsUnlocked = {}
     
-    # If none, execution stops here
-    if len(selections) == 0:
-        pc.confirmDialog(t="Error", message="Nothing selected.", icon='critical')
-    # Else, we batch set the primary visibility 
-    else:
-        print "Batch setting visibility to [%s]" % visibilityValue
-        print "Batch setting visible in Reflections to [%s]" % reflectValue
-        print "Batch setting visible in Refractions to [%s]" % refractValue
-        print "Batch setting cast Shadows to [%s]" % castShadValue
-        print "Batch setting receive Shadows to [%s]" % receiveShadValue
-        print "Batch setting opaque to [%s]" % opaqueValue
+    for element in elements:
+        # Check if controls are enabled and if so, append them to the list
+        state = pc.checkBox(uiWidgets[element], q=1, enable=1)
+        if (state):
+            elementsUnlocked[element] =  pc.checkBox(uiWidgets[element], q=1, value=1)
+            
+    print "Setting select objects to these render stats : %s" % elementsUnlocked
+    
+    # If everything is locked, we interrupt execution
+    if len(elementsUnlocked) == 0:
+        pc.confirmDialog(icon='critical', message="You have to unlock at least one render stat.")
+        exit('Please unlock at least one render stat.')
+    
+    # Saves what's selected and call the recursive dig & assign
+    selected = pc.ls(sl=1)
+    exploreFurther(selected, elementsUnlocked)
+
+def toggleEnable(control, *args):
+    
+    # Get the state of the control
+    state = pc.checkBox(uiWidgets[control], q=1, enable=1)
+    
+    if (state):
+        # If enabled, we grey it
+        state = pc.checkBox(uiWidgets[control], edit=1, enable=0)
+        # Button update
+        pc.button(uiWidgets[control+'Button'], edit=1, label='Unlock')
         
-        for sel in selections:
-            exploreFurther(sel, visibilityValue, opaqueValue, reflectValue, refractValue, castShadValue, receiveShadValue)
+    else:
+        state = pc.checkBox(uiWidgets[control], edit=1, enable=1)
+        pc.button(uiWidgets[control+'Button'], edit=1, label='Lock')
+        
 
 def showUI():
     # Delete windows if already existing
@@ -58,18 +64,34 @@ def showUI():
         pc.deleteUI("batchVisibility")
     
     # Main window
-    uiWidgets['window'] = pc.window("batchVisibility", menuBar=True, title="Batch set visibility", sizeable=False, h=430, w=300)
+    uiWidgets['window'] = pc.window("batchVisibility", menuBar=True, title="Render Stats", sizeable=False, h=430, w=300)
+    
+    uiWidgets['layout'] = pc.rowColumnLayout(nc=2)
+    
+    uiWidgets['primaryVisibility'] = pc.checkBox(label='Primary visibility', value=1)
+    uiWidgets['primaryVisibilityButton'] = pc.button(l='Lock', c=partial(toggleEnable, 'primaryVisibility'))
+    
+    uiWidgets['visibleInReflections'] = pc.checkBox(label='Visible in Reflections', value=1)
+    uiWidgets['visibleInReflectionsButton'] = pc.button(l='Lock', c=partial(toggleEnable, 'visibleInReflections'))
+    
+    uiWidgets['visibleInRefractions'] = pc.checkBox(label='Visible in Refractions', value=1)
+    uiWidgets['visibleInRefractionsButton'] = pc.button(l='Lock', c=partial(toggleEnable, 'visibleInRefractions'))
+    
+    uiWidgets['castsShadows'] = pc.checkBox(label='Cast shadows', value=1)
+    uiWidgets['castsShadowsButton'] = pc.button(l='Lock', c=partial(toggleEnable, 'castsShadows'))
+    
+    uiWidgets['receiveShadows'] = pc.checkBox(label='Receive shadows', value=1)
+    uiWidgets['receiveShadowsButton'] = pc.button(l='Lock', c=partial(toggleEnable, 'receiveShadows'))
+
+    pc.separator(h=10)
+    pc.separator(h=10)
+
+    uiWidgets['aiOpaque'] = pc.checkBox(label='Opaque', value=1)
+    uiWidgets['aiOpaqueButton'] = pc.button(l='Lock', c=partial(toggleEnable, 'aiOpaque'))
     
     uiWidgets['layout'] = pc.columnLayout()
-    
-    uiWidgets['visibility'] = pc.checkBox(label='Primary visibility', value=1)
-    uiWidgets['opaque'] = pc.checkBox(label='Opaque', value=1)
-    uiWidgets['visibleReflect'] = pc.checkBox(label='Visible in Reflections', value=1)
-    uiWidgets['visibleRefract'] = pc.checkBox(label='Visible in Refractions', value=1)
-    uiWidgets['castShadows'] = pc.checkBox(label='Cast shadows', value=1)
-    uiWidgets['receiveShadows'] = pc.checkBox(label='Receive shadows', value=1)
-    
-    pc.button(l='Apply to selected', c=applyToSelected)
+    pc.separator(h=10)
+    pc.button(l='Set all', c=applyToSelected)
     
     pc.showWindow(uiWidgets['window'])
     
